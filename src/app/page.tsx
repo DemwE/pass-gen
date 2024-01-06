d1616dc866b36859662a77790ac067164199bdef
DemwE <mateuszczarnecki360@gmail.com>
2024-01-06 14:04:15 +0100

    Changed include special characters and formatted files with biomejs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,15 +97,20 @@ export default function Home() {
 					/>
 				</div>
 				<div className="flex space-x-2 leading-5">
-					<label className="block text-md font-medium text-gray-700 dark:text-zinc-300">
-						Include Special Characters
+					<label className="block text-md font-medium text-gray-700 leading-5 dark:text-zinc-300">
+						{" "}
+						Special Characters
 					</label>
-					<input
-						type="checkbox"
-						className="block"
-						checked={includeSpecial}
-						onChange={handleCheckboxChange}
-					/>
+					<label className="relative inline-flex items-center mb-5 cursor-pointer">
+						<input
+							type="checkbox"
+							value=""
+							className="sr-only peer"
+							checked={includeSpecial}
+							onChange={handleCheckboxChange}
+						/>
+						<div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+					</label>
 				</div>
 			</div>
 		</main>
