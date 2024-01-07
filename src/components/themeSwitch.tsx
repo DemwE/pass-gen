948d196fba1540df16a823ea570a542d26a346bd
DemwE <mateuszczarnecki360@gmail.com>
2024-01-07 18:33:34 +0100

    Added animation to theme switch

--- src/components/themeSwitch.tsx
+++ src/components/themeSwitch.tsx
@@ -1,13 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 export default function ThemeSwitch() {
 	const [theme, setTheme] = useState("light");
+	const iconRef = useRef<HTMLElement | null>(null);
 
 	useEffect(() => {
 		if (theme === "dark") {
 			document.body.classList.add("dark");
 		} else {
 			document.body.classList.remove("dark");
+		}
+
+		if (iconRef.current) {
+			iconRef.current.classList.remove("animate-spin", "animate-once");
+		}
+
+		void iconRef.current?.offsetWidth;
+
+		if (iconRef.current) {
+			iconRef.current.classList.add("animate-spin", "animate-once");
 		}
 	}, [theme]);
 
@@ -20,11 +31,20 @@ export default function ThemeSwitch() {
 	};
 
 	return (
-		<div className="relative float-right bottom-2" onClick={toggleTheme}>
+		<div
+			className="relative float-right bottom-2 cursor-pointer"
+			onClick={toggleTheme}
+		>
 			{theme === "light" ? (
-				<i className="fa-regular fa-moon text-xl"></i>
+				<i
+					ref={iconRef}
+					className={`fa-regular fa-moon text-xl animate-spin animate-once animate-duration-200 animate-ease-in-out`}
+				></i>
 			) : (
-				<i className="fa-sharp fa-regular fa-sun-bright text-xl text-yellow-400"></i>
+				<i
+					ref={iconRef}
+					className={`fa-sharp fa-regular fa-sun-bright text-xl text-yellow-400 animate-spin animate-once animate-duration-200 animate-ease-in-out`}
+				></i>
 			)}
 		</div>
 	);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,8 @@ const config: Config = {
 
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated')
+  ],
 }
 export default config
